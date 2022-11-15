09f0c1b90d0beb78f92c649e65afc1a0541887c3
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-15 09:37:42 +0100

    Initial commit first

---
+++ app/src/main/java/com/barstool/matecki_algorytm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.barstool.matecki_algorytm
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.LinkedList
+import java.util.Queue
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }}
